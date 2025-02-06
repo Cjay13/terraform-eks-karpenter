4f0ca6dbcc0ca5c7c721025db725ba126e97a651
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-02-06 08:09:31 +0000

    Fixed syntax issues

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -27,14 +27,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "helm" = {
-  kubernetes = {
-    host                   = var.cluster_endpoint
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
+
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
