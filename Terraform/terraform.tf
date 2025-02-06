c89bcc497fec61d3836238e4836a6fa958881852
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-02-06 08:10:40 +0000

    Fixed syntax errors

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -46,7 +46,7 @@ provider "kubectl" {
   host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file = false
-  exec = {
+  exec {
    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
