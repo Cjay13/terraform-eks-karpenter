4c8c2999420c52ac9be315650f1b5687b995932a
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-02-07 06:08:33 +0000

    added nginx ingress controller

---
+++ Terraform/nginx-ingress.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "nginx-ingress-controller" {
+    name       = "ingress-nginx"
+    repository = "https://kubernetes.github.io/ingress-nginx"
+    chart      = "ingress-nginx"
+    namespace  = "ingress-nginx"
+    create_namespace = true
+
+    set {
+        name  = "controller.service.type"
+        value = "LoadBalancer"
+    }
+}
