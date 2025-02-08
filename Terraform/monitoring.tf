8b4cf055384b61ce3757cabbc96127506f3c3e4f
Charith Herath <64669020+Cjay13@users.noreply.github.com>
2025-02-08 02:53:44 +0000

    debugging

--- Terraform/monitoring.tf
+++ Terraform/monitoring.tf
@@ -2,7 +2,7 @@ resource "helm_release" "kube_prometheus_stack" {
   name       = "kube-prometheus-stack"
   namespace  = "monitoring"
   chart      = "prometheus-community/kube-prometheus-stack"
-  version    = "45.0.0"  # Ensure to use the appropriate version
+
 
   values = [
     <<-EOF
